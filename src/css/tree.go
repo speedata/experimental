2a34579ffd41cfddbd07703fc7372834c8b9f53a
Patrick Gundlach <gundlach@speedata.de>
2018-02-26 11:31:47 +0100

    Insert info about pages and font families in tree

--- src/css/tree.go
+++ src/css/tree.go
@@ -21,10 +21,19 @@ func stringValue(toks tokenstream) string {
 	ret := []string{}
 	for _, tok := range toks {
 		switch tok.Type {
-		case scanner.Ident, scanner.Dimension:
+		case scanner.Ident, scanner.Dimension, scanner.String:
 			ret = append(ret, tok.Value)
 		case scanner.Percentage:
 			ret = append(ret, tok.Value+"%")
+		case scanner.Hash:
+			ret = append(ret, "#"+tok.Value)
+		case scanner.Delim:
+			switch tok.Value {
+			case ";":
+				// ignore
+			default:
+				w("unhandled delimiter", tok)
+			}
 		default:
 			w("unhandled token", tok)
 		}
@@ -104,11 +113,7 @@ func fun(i int, sel *goquery.Selection) {
 		level++
 		sel.Contents().Each(fun)
 		level--
-		if level == 0 {
-			fmt.Println(lvindent, "}")
-		} else {
-			fmt.Println(lvindent, "},")
-		}
+		fmt.Println(lvindent, "},")
 	}
 }
 
@@ -124,8 +129,55 @@ func (c *CSS) dumpTree() {
 		}
 	}
 	elt := c.document.Find(":root > body")
-	fmt.Printf("htmltree = ")
+	fmt.Printf("csshtmltree = {\n")
+	c.dump_fonts()
+	c.dump_pages()
 	elt.Each(fun)
+	fmt.Println("}")
+}
+
+func (c *CSS) dump_pages() {
+	fmt.Println("  pages = {")
+	for k, v := range c.Pages {
+		if k == "" {
+			k = "*"
+		}
+		fmt.Printf("    [%q] = {\n", k)
+		m1k, m1v, m2k, m2v, m3k, m3v, m4k, m4v := fourValues("margin", v.margin)
+		fmt.Printf("       [%q]=%q, [%q]=%q,  [%q]=%q,  [%q]=%q,\n", m1k, m1v, m2k, m2v, m3k, m3v, m4k, m4v)
+		wd, ht := papersize(v.papersize)
+		fmt.Printf("       width = %q, height = %q,\n", wd, ht)
+		for paname, parea := range v.pagearea {
+			fmt.Printf("       [%q] = {\n", paname)
+			for _, rule := range parea {
+				fmt.Printf("           [%q] = %q ,\n", rule.Key, stringValue(rule.Value))
+			}
+			fmt.Println("       },")
+		}
+		fmt.Println("     },")
+	}
+
+	fmt.Println("  },")
+}
+
+func (c *CSS) dump_fonts() {
+	fmt.Println(" fontfamilies = {")
+	for name, ff := range c.Fontfamilies {
+		fmt.Printf("     [%q] = { regular = %q, bold=%q, bolditalic=%q, italic=%q },\n", name, ff.Regular, ff.Bold, ff.BoldItalic, ff.Italic)
+	}
+	fmt.Println(" },")
+}
+
+func fourValues(typ string, value string) (string, string, string, string, string, string, string, string) {
+	return typ + "-top", value, typ + "-left", value, typ + "-bottom", value, typ + "-right", value
+}
+
+func papersize(typ string) (string, string) {
+	switch typ {
+	case "a5":
+		return "148mm", "210mm"
+	}
+	return "210mm", "297mm"
 }
 
 func (c *CSS) openHTMLFile(filename string) error {
